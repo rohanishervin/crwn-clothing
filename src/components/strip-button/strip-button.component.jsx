2b3acd4d3073b19865479bc8e032b8b6fee48b8f
Shervin Rohani <shervin.rohani72@gmail.com>
2019-07-16 14:58:26 +0430

    updating stripe integration, updating our favicon and title

---
+++ src/components/strip-button/strip-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 1000;
+  const publishableKey = "pk_test_XF4EeaxOIP6sG5Ks9CgzWHtg00rtQO2pV5";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
